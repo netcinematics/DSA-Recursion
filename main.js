b0c1688fd9b5e5c51fb1f72683d6eff73514234f
Matt Thornberry <mtberry16@hotmail.com>
2020-03-04 09:17:19 -0700

    Add logs

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 function recursiveSumOf(list) {
   // Base Case -> exit condition
+  console.log("Top", "Exit condition = ", list.length);
   if (list.length === 1) {
     return list[0];
   }
 
   //Recursive case
+  console.log("Current list =", list);
   return list[0] + recursiveSumOf(list.slice(1));
 }
 
