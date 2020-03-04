db50546fbd12c031d14f6dc2142cebf527556130
NetCinematics <88969+netcinematics@users.noreply.github.com>
2020-03-04 09:24:37 -0700

    reformat
    reformat to get more precision. : )

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-//Example from materials
-
+//Sum numbers from start to end of list-.
 function recursiveSumOf(list) {
-  // Base Case -> exit condition
-  console.log("Top", "Exit condition = ", list.length);
-  if (list.length === 1) {
-    return list[0];
-  }
-
-  //Recursive case
-  console.log("Current list =", list);
-  return list[0] + recursiveSumOf(list.slice(1));
+  debugger;
+  if (list.length === 1){return list[0];} //escape-.
+  let listTail = list.slice(1); //slice from 1 to end-. 
+  let sum = list[0] + recursiveSumOf(listTail); //recursion-.
+  return sum;
 }
 
 let testList = [2, 4, 6, 8, 10];
+recursiveSumOf(testList);
